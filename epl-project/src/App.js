14c92c8bbf77e2a5a187bd37a81f303c3f5de7b3
HungTrinh99 <minhhung.it99@gmail.com>
2020-12-15 01:25:46 +0700

    First commit

---
+++ epl-project/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ĐỒ ÁN THIẾT KẾ GIAO DIỆN - PA5 - EPL NEWS</h1>
+    </div>
+  );
+}
+
+export default App;
