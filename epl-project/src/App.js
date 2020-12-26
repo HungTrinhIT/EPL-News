0358d875fba369291d56f64cf2b76ed7612ea2b6
HungTrinh99 <minhhung.it99@gmail.com>
2020-12-26 15:58:38 +0700

    update

--- epl-project/src/App.js
+++ epl-project/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Calender from "./components/calendar";
 function App() {
   return (
     <div className="App">
-      <h1>ĐỒ ÁN THIẾT KẾ GIAO DIỆN - PA5 - EPL NEWS</h1>
+      <Calender />
     </div>
   );
 }
